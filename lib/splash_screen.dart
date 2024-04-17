267515cc0c477be1ac269b4ed5207b036cd01f39
Divyansh Diya <Divyanshdiya24@gmail.com>
2024-04-18 00:32:53 +0530

    First AD

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:time_sheet_flutter_application/login_screen.dart';
 
-
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -31,8 +29,10 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Center(
         // Your splash screen logo widget
-        child: Image.asset(
-            'assets/image/icons8-flutter-192(-xxxhdpi).png'), // Replace with your splash screen logo asset
+        child: Image.network(
+          "https://www.liquidplanner.com/wp-content/uploads/2022/04/Release-Notes-Featured-Images-R27-Timesheets-1200-%C3%97-752-px-1024x642.png",
+          fit: BoxFit.fill,
+        ), // Replace with your splash screen logo asset
       ),
     );
   }
